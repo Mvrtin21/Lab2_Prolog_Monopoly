26c4b5ad0ffa702b6b9dec086a65871a517f713e
Mvrtin21 <martin69gamer@gmail.com>
2025-05-19 23:49:38 -0400

    Funcion de comprar y calcular renta de propiedades

---
+++ CAPITALIA-CODIGO-FUENTE_21781369_ArayaGaete/pruebas_21781369_Martin_ArayaGaete.pl
@@ -0,0 +1,7 @@
+% pruebas_21781369_Martin_ArayaGaete.pl
+
+:- consult('jugador_21781369_Martin_ArayaGaete.pl').
+:- consult('propiedad_21781369_Martin_ArayaGaete.pl').
+:- consult('carta_21781369_Martin_ArayaGaete.pl').
+:- consult('tablero_21781369_Martin_ArayaGaete.pl').
+:- consult('juego_21781369_Martin_ArayaGaete.pl').
