9ea22ea11b2b656b32cc2f4c9429cbd907df84e0
Mvrtin21 <martin69gamer@gmail.com>
2025-05-21 01:27:01 -0400

    Add juegoConstruirCasa and fix some predicates

--- CAPITALIA-CODIGO-FUENTE_21781369_ArayaGaete/propiedad_21781369_Martin_ArayaGaete.pl
+++ CAPITALIA-CODIGO-FUENTE_21781369_ArayaGaete/propiedad_21781369_Martin_ArayaGaete.pl
@@ -4,23 +4,23 @@ Desc: Representa a una propiedad en el juego.
 Estrategia: Se usa una estructura basada en listas para representar la propiedad.
 
 Argumentos:
-- Id (int): Identificador único de propiedad.
-- Nombre (string): Nombre de propiedad.
-- Precio (int): Precio de la propiedad.
-- Renta (int):  Renta de la propiedad.
-- Dueño (id_jugador/null): Propietario de propiedad.
-- casas (int): Cantidad de casas.
-- esHotel (boolean): Booleano para saber si es hotel o no.
-- estaHipotecada (boolean): Booleano para saber si esta hipotecada.
+%id (int)
+%nombre (string)
+%precio (int)
+%renta (int)
+%dueño (id_jugador/null)
+%casas (int)
+%esHotel (boolean)
+%estaHipotecada (boolean)
+%propiedad (property)
 
 Salida:
 - TdaPropiedad (list): Representación de propiedad como una lista de sus atributos.
    -> Se agrega Contador (int): Un contador inicializado en 0 para futuras funcionalidades.
 */
 
-
-propiedad(Id, Nombre, Renta, Dueño, Casas, EsHotel, EsHipotecada, TdaPropiedad) :-
-    TdaPropiedad = [Id, Nombre, Renta, Dueño, Casas, EsHotel, EsHipotecada].
+propiedad(Id, Nombre, Precio, Renta, Dueño, Casas, EsHotel, EsHipotecada, TdaPropiedad) :-
+    TdaPropiedad = [Id, Nombre, Precio, Renta, Dueño, Casas, EsHotel, EsHipotecada].
 
 /*               GETS              */
 
@@ -31,19 +31,22 @@ get_IdPropiedad([Id | _], Id).
 get_NombrePropiedad([_, Nombre | _], Nombre).
 
 % Se accede al tercer elemento de la lista de la propiedad.
-get_RentaPropiedad([_, _, Renta | _], Renta).
+get_PrecioPropiedad([_, _, Precio | _], Precio).
 
 % Se accede al cuarto elemento de la lista de la propiedad.
-get_DueñoPropiedad([_, _, _, Dueño | _], Dueño).
+get_RentaPropiedad([_, _, _, Renta | _], Renta).
 
 % Se accede al quinto elemento de la lista de la propiedad.
-get_CasasPropiedad([_, _, _, _, Casas | _], Casas).
+get_DueñoPropiedad([_, _, _, _, Dueño | _], Dueño).
 
 % Se accede al sexto elemento de la lista de la propiedad.
-get_EsHotelPropiedad([_, _, _, _, _, EsHotel | _], EsHotel).
+get_CasasPropiedad([_, _, _, _, _, Casas | _], Casas).
 
 % Se accede al séptimo elemento de la lista de la propiedad.
-get_EsHipotecadaPropiedad([_, _, _, _, _, _, EsHipotecada | _], EsHipotecada).
+get_EsHotelPropiedad([_, _, _, _, _, _, EsHotel | _], EsHotel).
+
+% Se accede al octavo elemento de la lista de la propiedad.
+get_EsHipotecadaPropiedad([_, _, _, _, _, _, _, EsHipotecada | _], EsHipotecada).
 
 /*               SETS              */
 
@@ -53,18 +56,20 @@ set_IdPropiedad([_ | Resto], NuevoId, [NuevoId | Resto]).
 % Se actualiza el segundo elemento (Nombre) de la lista de la propiedad.
 set_NombrePropiedad([X, _ | Resto], NuevoNombre, [X, NuevoNombre | Resto]).
 
-% Se actualiza el tercer elemento (Renta) de la lista de la propiedad.
-set_RentaPropiedad([X, Y, _ | Resto], NuevaRenta, [X, Y, NuevaRenta | Resto]).
+% Se actualiza el tercer elemento (Precio) de la lista de la propiedad.
+set_PrecioPropiedad([X, Y, _ | Resto], NuevoPrecio, [X, Y, NuevoPrecio | Resto]).
 
-% Se actualiza el cuarto elemento (Dueño) de la lista de la propiedad.
-set_DueñoPropiedad([X, Y, Z, _ | Resto], NuevoDueño, [X, Y, Z, NuevoDueño | Resto]).
+% Se actualiza el cuarto elemento (Renta) de la lista de la propiedad.
+set_RentaPropiedad([X, Y, Z, _ | Resto], NuevaRenta, [X, Y, Z, NuevaRenta | Resto]).
 
-% Se actualiza el quinto elemento (Casas) de la lista de la propiedad.
-set_CasasPropiedad([X, Y, Z, A, _ | Resto], NuevasCasas, [X, Y, Z, A, NuevasCasas | Resto]).
+% Se actualiza el quinto elemento (Dueño) de la lista de la propiedad.
+set_DueñoPropiedad([X, Y, Z, A, _ | Resto], NuevoDueño, [X, Y, Z, A, NuevoDueño | Resto]).
 
-% Se actualiza el sexto elemento (EsHotel) de la lista de la propiedad.
-set_EsHotelPropiedad([X, Y, Z, A, B, _ | Resto], NuevoEsHotel, [X, Y, Z, A, B, NuevoEsHotel | Resto]).
+% Se actualiza el sexto elemento (Casas) de la lista de la propiedad.
+set_CasasPropiedad([X, Y, Z, A, B, _ | Resto], NuevasCasas, [X, Y, Z, A, B, NuevasCasas | Resto]).
 
-% Se actualiza el séptimo elemento (EsHipotecada) de la lista de la propiedad.
-set_EsHipotecadaPropiedad([X, Y, Z, A, B, C, _ | Resto], NuevoEsHipotecada, [X, Y, Z, A, B, C, NuevoEsHipotecada | Resto]).
+% Se actualiza el séptimo elemento (EsHotel) de la lista de la propiedad.
+set_EsHotelPropiedad([X, Y, Z, A, B, C, _ | Resto], NuevoEsHotel, [X, Y, Z, A, B, C, NuevoEsHotel | Resto]).
 
+% Se actualiza el octavo elemento (EsHipotecada) de la lista de la propiedad.
+set_EsHipotecadaPropiedad([X, Y, Z, A, B, C, D, _ | Resto], NuevoEsHipotecada, [X, Y, Z, A, B, C, D, NuevoEsHipotecada | Resto]).
