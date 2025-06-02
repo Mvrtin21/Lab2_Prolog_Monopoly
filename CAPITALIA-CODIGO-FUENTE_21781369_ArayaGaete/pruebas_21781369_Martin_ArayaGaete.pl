d35dd8ad30ca9e7254b91f6b2a818f140055e391
Mvrtin21 <martin69gamer@gmail.com>
2025-06-01 21:52:00 -0400

    Avance informe y algunos ajustes en TDA juego y scripts

--- CAPITALIA-CODIGO-FUENTE_21781369_ArayaGaete/pruebas_21781369_Martin_ArayaGaete.pl
+++ CAPITALIA-CODIGO-FUENTE_21781369_ArayaGaete/pruebas_21781369_Martin_ArayaGaete.pl
@@ -1,6 +1,6 @@
 % pruebas_21781369_Martin_ArayaGaete.pl
 
-:- set_prolog_flag(answer_write_options, [max_depth(0)]).
+:- set_prolog_flag(answer_write_options, [max_depth(0), quoted(true), portray(true)]).
 
 :- consult('jugador_21781369_Martin_ArayaGaete.pl').
 :- consult('propiedad_21781369_Martin_ArayaGaete.pl').
